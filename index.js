a7735136c56b915ff87b91851feb89adaeb6bedd
Hain Thit <hainlthit@gmail.com>
2021-11-23 17:29:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [ "Lisa", "Kaitlin", "Jan"]
+function writeCards() {
+    let newArr = []
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return newArr
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+    
+}
